71c76189456bab1285b5e693e62d33f0bde81bb7
Axel Guichard <85789639+axelguichard@users.noreply.github.com>
2022-03-30 00:04:27 +0200

    Update Navbar.js

--- front/src/components/Navigation/Navbar.js
+++ front/src/components/Navigation/Navbar.js
@@ -111,22 +111,6 @@ const Navbar = ({ localUserId, isAdmin }) => {
           </ul>
         </nav>
         <div></div>
-        {/* <div className="round-svg">
-          <svg
-            className="arrondis-theme black-arrondis"
-            version="1.1"
-            id="Calque_1"
-            xmlns="http://www.w3.org/2000/svg"
-            xmlnsXlink="http://www.w3.org/1999/xlink"
-            x="0px"
-            y="0px"
-            viewBox="0 0 1440 48"
-            style={{ enableBackground: 'new 0 0 1440 48"' }}
-            xmlSpace="preserve"
-          >
-            <path d="M1440,48H0V0c0,0,205,47,720,47s720-46.9,720-46.9V48z"></path>
-          </svg>
-        </div> */}
       </div>
     </>
   );
